dfd92a3ab22875466ecf4ad665ffd58dc6f08e63
ahmad.aghazadeh <Ahmad.aghazadeh.a@gmail.com>
2023-09-01 09:38:02 +0330

    Add boot Spring files

---
+++ src/main/kotlin/com/github/ahmadaghazadeh/bootspring/controller/GreetingController.kt
@@ -0,0 +1,4 @@
+package com.github.ahmadaghazadeh.bootspring.controller
+
+class GreetingController {
+}
---
+++ src/test/kotlin/com/github/ahmadaghazadeh/bootspring/BootspringApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.ahmadaghazadeh.bootspring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BootspringApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
